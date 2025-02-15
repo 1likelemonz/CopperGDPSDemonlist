c3f856f55b6c1798e06b4555dc7b06fd6189eb24
1likelemonz <ilikecitronz@gmail.com>
2025-02-15 15:41:26 -0500

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -100,28 +100,22 @@ export default {
                     </template>
                     <h3>Submission Requirements</h3>
                     <p>
-                        Achieved the record without using hacks (however, FPS bypass is allowed, up to 360fps)
+                        Firstly, you have to record a video. Editing is fine, but the video has to show you hitting the end wall.
                     </p>
                     <p>
-                        Achieved the record on the level that is listed on the site - please check the level ID before you submit a record
+                        Secondly, you have to have audio of your clicks in the video. Meaning you have to have a mic.
                     </p>
                     <p>
-                        Have either source audio or clicks/taps in the video. Edited audio only does not count
+                        Thirdly, you do not have to have a handcam, but you can if you'd like.
                     </p>
                     <p>
-                        The recording must have a previous attempt and entire death animation shown before the completion, unless the completion is on the first attempt. Everyplay records are exempt from this
+                        Fourthly, you do not need a attempt counter active. If you have it active that is optional.
                     </p>
                     <p>
-                        The recording must also show the player hit the endwall, or the completion will be invalidated.
+                        Fifthly, if you are caught hacking or cheating your completions even once, you will be fully banned from the demonlist and none of your future levels will be rated.
                     </p>
                     <p>
-                        Do not use secret routes or bug routes
-                    </p>
-                    <p>
-                        Do not use easy modes, only a record of the unmodified level qualifies
-                    </p>
-                    <p>
-                        Once a level falls onto the Legacy List, we accept records for it for 24 hours after it falls off, then afterwards we never accept records for said level
+                        The submit record button does not work, there is a form on our discord server (https://discord.gg/QZJntJEVDC)
                     </p>
                 </div>
             </div>
